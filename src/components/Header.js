c61a920f233845e7184da3ccf74628cf87c7d8d6
Andrew Han <andrewthehan@gmail.com>
2021-01-18 03:37:28 -0800

    Add link to logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 export default function Header({ config, activated }) {
   return (
     <section className="header">
-      <img
-        className={activated ? "logo-activated" : "logo"}
-        src={config.logo}
-        alt="logo"
-        width="64"
-        height="64"
-      />
+      <a
+        href={config["logo-link"]}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Butterfly"
+        aria-label="logo link"
+      >
+        <img
+          className={activated ? "logo-activated" : "logo"}
+          src={config.logo}
+          alt="logo"
+          width="64"
+          height="64"
+        />
+      </a>
       <header>{config.name}</header>
     </section>
   );
