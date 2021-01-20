9190e23d3a3d5282277481e76c109f763cce9cd8
Andrew Han <andrewthehan@gmail.com>
2021-01-19 19:26:50 -0800

    Remove sheets icon.

--- src/Icons.js
+++ src/Icons.js
@@ -1,4 +1,3 @@
-export { CgPiano } from "@react-icons/all-files/cg/CgPiano";
 export { FaCode } from "@react-icons/all-files/fa/FaCode";
 export { FaLinkedinIn } from "@react-icons/all-files/fa/FaLinkedinIn";
 export { FaTwitch } from "@react-icons/all-files/fa/FaTwitch";
