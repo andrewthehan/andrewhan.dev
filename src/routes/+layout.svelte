6c761c97ffba4b983cda16c570097ebb34a1936a
Andrew Han <andrewthehan@gmail.com>
2024-12-08 02:43:50 -0800

    Add a music page.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import "../app.css";
+
+  export let children: Snippet;
+</script>
+
+<svelte:head>
+  <title>Andrew Han</title>
+  <meta name="description" content="Andrew Han's personal site" />
+
+  <meta property="og:url" content="https://andrewhan.dev/" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Andrew Han" />
+  <meta property="og:description" content="Andrew Han's personal site" />
+  <meta
+    property="og:image"
+    content="https://raw.githubusercontent.com/andrewthehan/butterfly/master/butterfly.png"
+  />
+</svelte:head>
+
+<div class="app">
+  <header>
+    <a href="/" class="home">
+      <img
+        class="logo"
+        src="https://raw.githubusercontent.com/andrewthehan/butterfly/master/butterfly.png"
+        alt="Andrew Han"
+        width="32"
+        height="32"
+      />
+      <h1 class="name">Andrew Han</h1>
+    </a>
+  </header>
+
+  <main>
+    <div class="main-content">
+      {@render children()}
+    </div>
+  </main>
+</div>
+
+<style>
+  .app {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    height: 100vh;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+
+    padding: 12px 0;
+    width: 768px;
+    max-width: 90vw;
+  }
+
+  .home {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+  }
+
+  .logo {
+    border-radius: 50%;
+  }
+
+  .name {
+    margin: 0 1rem;
+  }
+
+  main {
+    width: 100%;
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    overflow-y: auto;
+  }
+
+  .main-content {
+    display: flex;
+    flex-direction: column;
+
+    width: 768px;
+    max-width: 90vw;
+
+    height: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
   import Piano from "lucide-svelte/icons/piano";
   import Twitch from "lucide-svelte/icons/twitch";
   import "../app.css";
-  import Link from "./Link.svelte";
+  import Link from "$lib/Link.svelte";
 
   const socials = [
     {
@@ -72,105 +72,51 @@
   ];
 </script>
 
-<svelte:head>
-  <title>Andrew Han</title>
-  <meta name="description" content="Andrew Han's personal site" />
+<section>
+  <p>
+    I am a software engineer with a passion for code design, games, and <a href="/music">music</a>.
+  </p>
 
-  <meta property="og:url" content="https://andrewhan.dev/" />
-  <meta property="og:type" content="website" />
-  <meta property="og:title" content="Andrew Han" />
-  <meta property="og:description" content="Andrew Han's personal site" />
-  <meta
-    property="og:image"
-    content="https://raw.githubusercontent.com/andrewthehan/butterfly/master/butterfly.png"
-  />
-</svelte:head>
+  <div class="buttons">
+    <a href="https://ko-fi.com/andrewthehan" target="_blank" rel="noopener noreferrer">
+      <img
+        height="36"
+        src="https://storage.ko-fi.com/cdn/widget/kofi_new_button.png"
+        alt="Tip me at ko-fi.com"
+      />
+    </a>
+  </div>
+</section>
 
-<div class="app">
-  <header>
-    <img
-      class="logo"
-      src="https://raw.githubusercontent.com/andrewthehan/butterfly/master/butterfly.png"
-      alt="Andrew Han"
-      width="32"
-      height="32"
-    />
-    <h1 class="name">Andrew Han</h1>
-  </header>
+<section>
+  <h2>Socials</h2>
+  {#each socials as { name, description, link, alertMessage, icon: Icon }, i}
+    <Link {name} {description} {link} {alertMessage}>
+      {#snippet icon()}
+        <Icon />
+      {/snippet}
+    </Link>
+    {#if i < socials.length - 1}
+      <hr />
+    {/if}
+  {/each}
+</section>
 
-  <section>
-    <p>I am a software engineer with a passion for code design, music, and games.</p>
-
-    <div class="buttons">
-      <a href="https://ko-fi.com/andrewthehan" target="_blank" rel="noopener noreferrer">
-        <img
-          height="36"
-          src="https://storage.ko-fi.com/cdn/widget/kofi_new_button.png"
-          alt="Tip me at ko-fi.com"
-        />
-      </a>
-    </div>
-  </section>
-
-  <section>
-    <h2>Socials</h2>
-    {#each socials as { name, description, link, alertMessage, icon: Icon }, i}
-      <Link {name} {description} {link} {alertMessage}>
-        {#snippet icon()}
-          <Icon />
-        {/snippet}
-      </Link>
-      {#if i < socials.length - 1}
-        <hr />
-      {/if}
-    {/each}
-  </section>
-
-  <section>
-    <h2>Projects</h2>
-    {#each projects as { name, description, link, icon: Icon }, i}
-      <Link {name} {description} {link}>
-        {#snippet icon()}
-          <Icon />
-        {/snippet}
-      </Link>
-      {#if i < projects.length - 1}
-        <hr />
-      {/if}
-    {/each}
-  </section>
-</div>
+<section>
+  <h2>Projects</h2>
+  {#each projects as { name, description, link, icon: Icon }, i}
+    <Link {name} {description} {link}>
+      {#snippet icon()}
+        <Icon />
+      {/snippet}
+    </Link>
+    {#if i < projects.length - 1}
+      <hr />
+    {/if}
+  {/each}
+</section>
 
 <style>
-  :root {
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-  }
-
-  .app {
-    display: flex;
-    flex-flow: column;
-
-    width: 768px;
-    max-width: 90vw;
-  }
-
-  header {
-    display: flex;
-    align-items: center;
-
-    margin: 12px 0;
-  }
-
-  .logo {
-    border-radius: 50%;
-  }
-
-  .name {
-    margin: 0 1rem;
-  }
-
   section {
     margin: 6px 0;
   }
